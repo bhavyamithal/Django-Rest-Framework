00db6c3a8f328b4f00c6782025578a43e938ed12
bhavyamithal <bm820@snu.edu.in>
2023-03-28 22:05:19 +0530

    django model instance to dictionary

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,12 @@
 import json
 from django.http import JsonResponse
 from products.models import Product
+from django.forms.models import model_to_dict
+
 
 def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
-    data['id'] = model_data.id
-    data['title'] = model_data.title
-    data['description'] = model_data.description
-    data['price'] = model_data.price
-    # data['summary'] = model_data.summary
-    # data['featured'] = model_data.featured
+    if model_data:
+        data = model_to_dict(model_data, fields = ['title', 'description', 'price'])
     return JsonResponse(data)
