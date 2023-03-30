9db82063ca7c85419a2dd8a69fba3bc5288055a2
bhavyamithal <bm820@snu.edu.in>
2023-03-30 20:22:31 +0530

    DRF Generics Retrieve API View

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from products.models import Product
+from products.serializers import ProductSerializer
+
+
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+product_detail_api_view = ProductDetailAPIView.as_view()
